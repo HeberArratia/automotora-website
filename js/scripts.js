df24a725218c5d77ee82925bd550d42b924eaacf
Heber Arratia <heber.im87@gmail.com>
2017-11-12 00:38:39 -0300

    se anade info y estilos a vista auto individual

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
 	$(window).load(function(){
       $('.preloader').fadeOut(1000); // set duration in brackets    
     });
+
+	$("#lightgallery").lightGallery(); 
+
+
     /*
 
 	if ($(window).width() > 960) {
