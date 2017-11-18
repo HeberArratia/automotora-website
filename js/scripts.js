c463e441ba546ef8c81ebba80a7313a50d831356
Heber Arratia <heber.im87@gmail.com>
2017-11-18 00:51:49 -0300

    se crean otras vistas y se crea navegacion a formulario consignacion en menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$('.carousel').flickity({
 		  // options
 		autoPlay: 3000
@@ -8,36 +9,10 @@ $(document).ready(function(){
       $('.preloader').fadeOut(1000); // set duration in brackets    
     });
 
+    $('#navbar3').navScroll();
+
 	$("#lightgallery").lightGallery(); 
 
-
-    /*
-
-	if ($(window).width() > 960) {
-	   $('#menu').css({'display' : 'block'});
-	}
-	else {
-	   //alert('More than 960');
-	}
+	
     
-    $(window).scroll(function() {
-	    if($(window).scrollTop() > 400){
-	    	$('#menu').css({'opacity' : '1'});
-	    } else {
-	    	$('#menu').css({'opacity' : '0'});
-	    }
-    });
-
-   $("#modal").iziModal({
-	  headerColor: '#FF5766', //ヘッダー部分の色
-	  width: '50%', //横幅
-	  overlayColor: 'rgba(0, 0, 0, 0.5)', //モーダルの背景色
-	  transitionIn: 'fadeInUp', //表示される時のアニメーション
-	  transitionOut: 'fadeOutDown' //非表示になる時のアニメーション
-	});
-
-   $(document).on('click', '.trigger', function (event) {
-    	event.preventDefault();
-    	$('#modal').iziModal('open');
-	});*/
 });
